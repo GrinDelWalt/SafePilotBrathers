cfb842e24b088c297c91235f1879e2d7cf45fc5e
Grindelwalt <Sulxj600s@gmail.com>
2022-10-07 02:04:19 +0300

    Добавьте файлы проекта.

---
+++ SafePilotBrathers/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafePilotBrathers.ViewModels.Base
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanget([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
