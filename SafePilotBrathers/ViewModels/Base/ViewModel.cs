f67ae2306512e8d8bc209df4fc99e0cb4c1e3f97
Grindelwalt <Sulxj600s@gmail.com>
2022-10-07 02:07:56 +0300

    Подготовка патерна MVVM создание ViewModel

--- SafePilotBrathers/ViewModels/Base/ViewModel.cs
+++ SafePilotBrathers/ViewModels/Base/ViewModel.cs
@@ -16,5 +16,12 @@ namespace SafePilotBrathers.ViewModels.Base
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
         }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanget(PropertyName);
+            return true;
+        }
     }
 }
