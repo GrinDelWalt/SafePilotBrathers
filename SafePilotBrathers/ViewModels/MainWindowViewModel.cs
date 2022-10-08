5916d904feb4a44d21f321d699bd6e56106321f7
Grindelwalt <Sulxj600s@gmail.com>
2022-10-09 01:20:41 +0300

    привязка кнопки к событию, привязка текста к свойству

--- SafePilotBrathers/ViewModels/MainWindowViewModel.cs
+++ SafePilotBrathers/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,10 @@
-﻿using SafePilotBrathers.Models;
+﻿using SafePilotBrathers.Infrastructure.Commands;
+using SafePilotBrathers.Models;
 using SafePilotBrathers.ViewModels.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Windows;
+using System.Windows.Input;
 
 namespace SafePilotBrathers.ViewModels
 {
@@ -18,10 +23,37 @@ namespace SafePilotBrathers.ViewModels
                 Set(ref _title, value);
             }
         }
+        public int SizeSafe
+        {
+            get => _safe.Size;
+            
+            set
+            {
+                if (value > 2 & value < 8)
+                {
+                    _safe.Size = value;
+                    OnPropertyChanget();
+                }
+                else
+                {
+                    MessageBox.Show("Размер сейфа не может быть меньше 3 и больше 7", "Внимание",MessageBoxButton.OK);
+                }
+            }  
+        }
+
+        public ICommand SettingSizeSafeCommand { get; }
+        private bool CanSettingSizeSafeCommandExecute(object p) => true;
+
+        private void OnSettingSizeSafeCommandExecute(object p)
+        {
+            
+        }
         public MainWindowViewModel()
         {
+            SettingSizeSafeCommand = new LambdaCommand(OnSettingSizeSafeCommandExecute, CanSettingSizeSafeCommandExecute);
             _title = "Сейф братьев Пилотов";
             _safe = new Safe();
+            _safe.Size = 4;
         }
 
         public void SafeDimensions()
