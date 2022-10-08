98450387271977d5f109a545f600cfa430920384
Grindelwalt <Sulxj600s@gmail.com>
2022-10-08 21:08:41 +0300

    Привязка титула окна

---
+++ SafePilotBrathers/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,29 @@
+﻿using SafePilotBrathers.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SafePilotBrathers.Infrastructure.Commands
+{
+    internal class LambdaCommand : Command
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+        public LambdaCommand()
+        {
+
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke(parameter) ?? true;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
--- SafePilotBrathers/ViewModels/MainWindowViewModel.cs
+++ SafePilotBrathers/ViewModels/MainWindowViewModel.cs
@@ -1,25 +1,32 @@
 ﻿using SafePilotBrathers.Models;
 using SafePilotBrathers.ViewModels.Base;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SafePilotBrathers.ViewModels
 {
     public class MainWindowViewModel : ViewModel
     {
+        private string _title;
         private Safe _safe;
-
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                //    if (Equals(_title, value)) return;
+                //    _title = value;
+                //    OnPropertyChanget();
+                Set(ref _title, value);
+            }
+        }
         public MainWindowViewModel()
         {
+            _title = "Сейф братьев Пилотов";
             _safe = new Safe();
         }
 
         public void SafeDimensions()
         {
-            
+
         }
     }
 }
